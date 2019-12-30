9d00cda4a85ec0cb9da12de15457a5828c55c4f2
sinmetal <metal.tie@gmail.com>
2019-12-30 13:59:10 +0900

    bucketを指定して実行できるようにした refs #3

--- main.go
+++ main.go
@@ -1,42 +1,38 @@
 package main
 
 import (
+	"context"
 	"flag"
 	"fmt"
 	"os"
-
-	"github.com/k0kubun/pp"
 )
 
 func main() {
 	var (
-		dir = flag.String("dir", ".", "target file path")
+		dir    = flag.String("dir", ".", "target file path")
+		bucket = flag.String("bucket", "hoge", "upload gcs bucket")
 	)
 	flag.Parse()
 	fmt.Println(*dir)
+	fmt.Println(*bucket)
 
 	l, err := ListFiles(*dir)
 	if err != nil {
 		fmt.Printf("failed list files... %+v\n", err)
 		os.Exit(1)
 	}
-	_, err = pp.Println(l)
+
+	ctx := context.Background()
+
+	gcs, err := NewStorageService(ctx)
 	if err != nil {
-		fmt.Printf("failed k0kubun pp... %+v\n", err)
+		fmt.Printf("failed NewStorageService... %+v\n", err)
 		os.Exit(1)
 	}
 
-	//ctx := context.Background()
-	//
-	//gcs, err := NewStorageService(ctx)
-	//if err != nil {
-	//	fmt.Printf("failed NewStorageService... %+v\n", err)
-	//	os.Exit(1)
-	//}
-
-	//	w := gcs.NewWriter(ctx, "gcpug-nouhau-dev", "")
-	//w.ObjectAttrs.ContentType = "text/csv; charset=utf-8"
-	//w.ObjectAttrs.ContentDisposition = fmt.Sprintf(`attachment;filename="%v-%v.csv"`, runRequest.ID, stmt.ID)
-
-	// io.Copy().,0
+	c := NewConverter(gcs)
+	if err := c.Run(ctx, *bucket, l); err != nil {
+		fmt.Printf("failed Converter.Run... %+v\n", err)
+		os.Exit(1)
+	}
 }
